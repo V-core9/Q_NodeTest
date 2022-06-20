a0fdbf5429adb8dfc96ff75c0dfa24f56862765e
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-06-20 11:45:27 +0200

    INIT things generated and set

---
+++ api/src/api/users/users.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { isAuthenticated, isAdmin } = require('../../middlewares');
+const { findUserById, listUsers, updateUser } = require('./users.services');
+
+const router = express.Router();
+
+router.get('/', isAuthenticated, async (req, res, next) => {
+  try {
+    res.json(await listUsers());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/', isAuthenticated, isAdmin, async (req, res, next) => {
+  try {
+    res.json(await updateUser(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ api/src/api/users/users.services.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const { db } = require('../../utils/db');
+
+function findUserByEmail(email) {
+  return db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+}
+
+function createUserByEmailAndPassword(user) {
+  user.password = bcrypt.hashSync(user.password, 12);
+  return db.user.create({
+    data: user,
+  });
+}
+
+function findUserById(id) {
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+function listUsers() {
+  return db.user.findMany();
+}
+
+function updateUser(data) {
+  return db.user.update({
+    where: {
+      id: data.id,
+    },
+    data: {
+      email: data.email,
+    }
+  });
+}
+
+module.exports = {
+  listUsers,
+  findUserByEmail,
+  findUserById,
+  updateUser,
+  createUserByEmailAndPassword
+};
