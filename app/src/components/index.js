1bf66a23f165411ce12f4f2aad351ac0666a4950
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-06-30 20:51:13 +0200

    yea bunch of random stuff added

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -3,3 +3,5 @@ export * from './Nav';
 export * from './Footer';
 export * from './PrivateRoute';
 export * from './PrivateRouteAdmin';
+export * from './NewBookModal';
+export * from './NewBookForm';
--- app/src/pages/Users.js
+++ app/src/pages/Users.js
@@ -17,19 +17,35 @@ function Users() {
     }, []);
 
     return (
-        <div>
+        <div className="adminUsers">
             <h1>Hi {user?.username}!</h1>
             <p>This is an example Admin Page</p>
             <h3>Users from secure api end point:</h3>
             {users.length &&
-                <ul>
+                <ul className="list-group">
                     {users.map(user =>
-                        <li key={user.id}>{user.email} {user.isAdmin}</li>
+                        <li className="list-group-item" key={user.id}>
+                            <div className="card">
+                                <img className="card-img-top" src='/logo192.png' alt={user.username} />
+                                <div className="card-body" title={user.username}>
+                                    <div className="d-flex w-100 justify-content-between">
+                                        <h5 className="mb-1">{user.username}</h5>
+                                        <small>
+                                            <button className="btn-secondary" onClick={() => console.log("I will EDIT User:", user.id)}>🎨 Edit</button>
+                                            <button className="btn-danger" onClick={() => console.log("I will DELETE User:", user.id)} title="Delete Book">❌ Delete</button>
+                                        </small>
+                                    </div>
+                                    <p className="mb-0">🆔 {user.id}</p>
+                                    <p className="mb-1">📧 {user.email}</p>
+                                    <small>Account Type: {user.isAdmin ? '👨‍💻 Admin' : '👤 User'}</small>
+                                </div>
+                            </div>
+                        </li>
                     )}
                 </ul>
             }
             {users.loading && <div className="spinner-border spinner-border-sm"></div>}
             {users.error && <div className="text-danger">Error loading users: {users.error.message}</div>}
-        </div>
+        </div >
     );
 }
